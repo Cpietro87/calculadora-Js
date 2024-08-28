1ad6df4832239f6de280b47df439757d08da96b4
Cpietro87 <cristianpietrobon20192@gmail.com>
2024-08-28 15:07:35 -0300

    Juego de adivinanza

---
+++ todolist.js
@@ -0,0 +1,26 @@
+document.getElementById('add-task-btn').addEventListener('click', function () {
+    const taskInput = document.getElementById('task-input');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const taskList = document.getElementById('task-list');
+
+        const listItem = document.createElement('li');
+        listItem.textContent = taskText;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Eliminar';
+
+        deleteBtn.addEventListener('click', function () {
+            taskList.removeChild(listItem);
+        });
+        
+
+        listItem.appendChild(deleteBtn);
+        taskList.appendChild(listItem);
+
+        taskInput.value = '';
+        taskInput.focus();
+    }
+})
+
