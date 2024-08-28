61863e26048b18e756aacde18c277d23490778f4
Cpietro87 <cristianpietrobon20192@gmail.com>
2024-08-28 15:44:01 -0300

    función de adivinanza

---
+++ adivinanza.js
@@ -0,0 +1,11 @@
+// esta variable es para generar un número random del 1 al 100
+let numAzar = Math.floor(Math.random()*100) + 1;
+// obtenemos el número que ingresa el usuario
+let numEntrada = document.getElementById('numeroEntrada');
+let mensaje = document.getElementById('mensaje');
+
+
+function chaquearResultado() {
+    
+}
+
